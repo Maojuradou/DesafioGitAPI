c03cf7f8c6479962af71b4a6cfc8587d35efcb4c
Mauricio Jurado <mauriciojurado10@gmail.com>
2026-09-08 10:32:08 -0500

    C3: agregar consulta de estudiante

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +11,30 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+var estudiantes = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Estudiante(1, "Carlos Pérez", "3001234567"),
+    new Estudiante(2, "María López", "3109876543"),
+    new Estudiante(3, "Juan García", "3205551234")
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/estudiantes", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return estudiantes;
 })
-.WithName("GetWeatherForecast");
+.WithName("GetEstudiantes");
+
+app.MapGet("/estudiantes/{id}", (int id) =>
+{
+    var estudiante = estudiantes.FirstOrDefault(e => e.Id == id);
+
+    if (estudiante is null)
+        return Results.NotFound();
+
+    return Results.Ok(estudiante);
+})
+.WithName("GetEstudiantePorId");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+record Estudiante(int Id, string Nombre, string Telefono);
